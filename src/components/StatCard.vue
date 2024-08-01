64c8c4ce4bb226b47e2e06b4a96fba2ad97c2ede
jojohahana <yohana.chriezt@gmail.com>
2024-08-01 08:59:38 +0700

    add stat cards on top charts

---
+++ src/components/StatCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+      <div :class="iconBgClass" class="p-4">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path :d="iconPath" />
+        </svg>
+      </div>
+      <div class="px-4 text-gray-700">
+        <h3 class="text-sm tracking-wider">{{ title }}</h3>
+        <p class="text-3xl">{{ value }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'StatCard',
+    props: {
+      iconPath: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      value: {
+        type: String,
+        required: true
+      },
+      iconBgClass: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,35 @@
 <template>
   <div>
-    <!-- <h1>Home Page</h1>
-    <p>Welcome to the Home page.</p> -->
+    <!-- Start Various Cards  -->
+    <div class="grid grid-cols-1 gap-4 px-4 mt-8 sm:grid-cols-4 sm:px-8 mb-8">
+      <StatCard
+        iconPath="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+        title="Total Member"
+        value="12,768"
+        iconBgClass="bg-green-400"
+      />
+      <StatCard
+        iconPath="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
+        title="Total Post"
+        value="39,265"
+        iconBgClass="bg-blue-400"
+      />
+      <StatCard
+        iconPath="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+        title="Total Comment"
+        value="142,334"
+        iconBgClass="bg-indigo-400"
+      />
+      <StatCard
+        iconPath="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+        title="Server Load"
+        value="34.12%"
+        iconBgClass="bg-red-400"
+      />
+    </div>
+    <!-- End Various Cards  -->
+
+    <!-- Start Various Charts  -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <div class="card">
         <LineChart />
@@ -13,6 +41,9 @@
         <PieChart />
       </div>
     </div>
+    <!-- End Various Charts  -->
+
+
   </div>
 </template>
 
@@ -20,12 +51,14 @@
 import LineChart from './charts/LineChart.vue';
 import BarChart from './charts/BarChart.vue';
 import PieChart from './charts/PieChart.vue';
+import StatCard from '../components/StatCard.vue';
 
 export default {
   components: {
     LineChart,
     BarChart,
-    PieChart
+    PieChart,
+    StatCard
   },
   name: 'HomePage'
 }
